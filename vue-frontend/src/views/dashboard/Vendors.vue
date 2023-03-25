2b0280269cdcaea93b684608f094581cb5d6381b
Chandan Ojha <chandanojha776@gmail.com>
2023-03-25 17:33:39 +0600

    `vendors crud on development`

--- vue-frontend/src/views/dashboard/Vendors.vue
+++ vue-frontend/src/views/dashboard/Vendors.vue
@@ -3,7 +3,8 @@
     <h2>All Vendors</h2>
     <TheButton @click="addModal = true">Add New</TheButton>
   </div>
-  <table class="mt-4">
+  <div class="text-center" v-if="gettingVendors">Loading....</div>
+  <table class="mt-4" v-else>
     <thead>
       <tr>
         <th>Name</th>
@@ -12,9 +13,9 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="i in 9" :key="i">
-        <td>Pencilium</td>
-        <td>Pencilium Des</td>
+      <tr v-for="vendor in vendors" :key="vendor.name">
+        <td>{{ vendor.name }}</td>
+        <td>{{ vendor.description }}</td>
         <td>
           <img src="/img/edit.png" alt="" class="action-icon" />
           <img
@@ -27,20 +28,108 @@
     </tbody>
   </table>
   <TheModal v-model="addModal" heading="Add new vendor">
-    <h2>Enter Vendor Details</h2>
+    <form @submit.prevent="addNew">
+      <label class="block">Vendor Name</label>
+      <input
+        type="text"
+        placeholder="Enter vendor name"
+        class="mt-1 w-100"
+        required
+        v-model="newVendor.name"
+      />
+      <label class="block mt-3">Description</label>
+      <input
+        type="text"
+        placeholder="Write short description"
+        class="mt-1 w-100"
+        required
+        v-model="newVendor.description"
+      />
+      <TheButton :loading="adding" class="w-100 mt-4"> Add </TheButton>
+    </form>
   </TheModal>
 </template>
 
 <script>
+import axios from "axios";
 import TheButton from "../../components/TheButton.vue";
 import TheModal from "../../components/TheModal.vue";
 export default {
   data: () => ({
     addModal: false,
+    newVendor: {
+      name: "",
+      description: "",
+    },
+    adding: false,
+    vendors: [],
+    gettingVendors: false,
   }),
   components: {
     TheButton,
     TheModal,
+  },
+  methods: {
+    resetForm() {
+      this.newVendor = { name: "", description: "" };
+    },
+    getAllvendors() {
+      this.gettingVendors = true;
+      axios
+        .get("http://127.0.0.1:8000/api/get-vendor-list", {
+          headers: {
+            authorization: localStorage.getItem("accessToken"),
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          this.vendors = res.data.vendor_list;
+        })
+        .catch((err) => {
+          let errorMessage = "Something went wrong";
+          if (err.response) {
+            errorMessage = err.response.data.message;
+          }
+          this.$eventBus.emit("toast", {
+            type: "Error",
+            message: errorMessage,
+          });
+        })
+        .finally(() => {
+          this.gettingVendors = false;
+        });
+    },
+    addNew() {
+      //console.log(localStorage.getItem("accessToken"));
+      this.adding = true;
+      axios
+        .post("http://127.0.0.1:8000/api/create-vendor", this.newVendor, {
+          headers: {
+            authorization: localStorage.getItem("accessToken"),
+          },
+        })
+        .then((res) => {
+          this.$eventBus.emit("toast", {
+            type: "Success",
+            message: res.data.message,
+          });
+          this.addModal = false;
+          this.resetForm();
+        })
+        .catch((err) => {
+          let errorMessage = "Something went wrong";
+          if (err.response) {
+            errorMessage = err.response.data.message;
+          }
+          this.$eventBus.emit("toast", {
+            type: "Error",
+            message: errorMessage,
+          });
+        })
+        .finally(() => {
+          this.adding = false;
+        });
+    },
   },
 };
 </script>
