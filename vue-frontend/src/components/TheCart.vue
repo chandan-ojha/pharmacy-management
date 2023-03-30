961058b5c08f61eb29451a0231171c6f0e37fe6d
Chandan Ojha <chandanojha776@gmail.com>
2023-03-30 10:48:34 +0600

    `cart component design done`

---
+++ vue-frontend/src/components/TheCart.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="the-cart">
+    <div class="the-cart__heading">Cart Items</div>
+    <table>
+      <tr>
+        <th>Item</th>
+        <th>Weight</th>
+        <th>Price</th>
+        <th>Qty</th>
+        <th>Total</th>
+        <th>
+          <img
+            src="/img/trash.png"
+            class="action-icon action-icon--delete-small"
+            alt=""
+          />
+        </th>
+      </tr>
+      <tr v-for="i in 5" :key="i">
+        <td>Name</td>
+        <td>50 mg</td>
+        <td>30</td>
+        <td>
+          <input class="qty-input" type="number" />
+        </td>
+        <td class="text-right">500</td>
+        <td>
+          <img
+            src="/img/trash.png"
+            class="action-icon action-icon--delete-small"
+            alt=""
+          />
+        </td>
+      </tr>
+      <tr>
+        <td colspan="6">
+          <div class="text-right">
+            <hr />
+            <strong>Grand Total : 1000 </strong>
+          </div>
+        </td>
+      </tr>
+    </table>
+    <TheButton class="w-100 mt-4">Checkout</TheButton>
+  </div>
+</template>
+
+<script>
+import TheButton from "./TheButton.vue";
+export default {
+  components: {
+    TheButton,
+  },
+};
+</script>
+
+<style>
+.the-cart {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 11px 22px;
+}
+
+.the-cart__heading {
+  font-size: 25px;
+  font-weight: bold;
+  border-bottom: 1px solid rgb(173, 173, 173);
+}
+
+.qty-input {
+  width: 50px;
+  padding: 5px 7px !important;
+}
+
+.action-icon--delete-small {
+  height: 14px !important;
+}
+</style>
--- vue-frontend/src/views/Dashboard.vue
+++ vue-frontend/src/views/Dashboard.vue
@@ -11,17 +11,34 @@
         <RouterView></RouterView>
       </div>
     </div>
+    <div
+      class="dashboard__cart"
+      :class="{ 'dashboard__cart--opened': cartOpened }"
+    >
+      <div class="the-cart-container">
+        <TheCart @close="cartOpened = false"></TheCart>
+      </div>
+
+      <div class="cart-arrow" @click="cartOpened = !cartOpened">
+        <img src="/img/arrow.png" alt="" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import TheSidebar from "../components/TheSidebar.vue";
 import TheHeader from "../components/TheHeader.vue";
+import TheCart from "../components/TheCart.vue";
 import { setPrivateHeaders } from "../service/axiosInstance";
 export default {
+  data: () => ({
+    cartOpened: false,
+  }),
   components: {
     TheSidebar,
     TheHeader,
+    TheCart,
   },
   created() {
     const accessToken = localStorage.getItem("accessToken");
@@ -73,5 +90,53 @@ export default {
   padding: 22px 33px;
   background-color: rgb(235, 235, 235);
   overflow-y: auto;
+}
+
+.dashboard__cart {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: -444px;
+  width: 444px;
+  box-shadow: 0 0 42px 14px rgb(0 0 0 / 22%);
+  z-index: 999999;
+  background-color: rgb(255 255 255);
+  display: flex;
+  transition: all 0.25s;
+}
+
+.dashboard__cart--opened {
+  right: 0;
+}
+
+.the-cart-container {
+  position: relative;
+  flex: 1;
+  display: flex;
+}
+
+.cart-arrow {
+  width: 44px;
+  height: 55px;
+  background-color: #fff; /* var(--brand-color);*/
+  color: #fff;
+  position: absolute;
+  top: 44%;
+  left: -44px;
+  border-radius: 6px 0 0 6px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.cart-arrow img {
+  transition: all 0.25s;
+  width: 22px;
+  transform: rotate(180deg);
+}
+
+.dashboard__cart--opened .cart-arrow img {
+  transform: rotate(0deg);
 }
 </style>
